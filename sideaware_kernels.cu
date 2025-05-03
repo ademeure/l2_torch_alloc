a28aead1f0be61a72ae155fd68b5eee148e623cf
ademeure <arundemeure@gmail.com>
2025-05-03 09:16:24 +0000

    Full cache streaming, discard, and reverse order support... but inner loop instructions have increased compared to a few commits ago, and I need to debug why

--- sideaware_kernels.cu
+++ sideaware_kernels.cu
@@ -30,38 +30,36 @@ struct __align__(16) packed {
     __device__ __forceinline__ const T& operator[](int index) const { return data[index]; }
 };
 
-template<bool last_read = true, typename T, size_t N>
-__device__ __forceinline__ packed<T, N> load_from(const packed<T, N> *src) {
-    // TODO: why does this result in ".STRONG" on the load?!
-    if constexpr (last_read) {
-        int4 data;
+template<bool evict = false, typename T, size_t N>
+__device__ __forceinline__ packed<T, N> load(const packed<T, N> * __restrict__ src) {
+    if constexpr (evict) {
         if constexpr (sizeof(T) * N == 16) {
-            asm volatile("ld.weak.global.v4.b32.cg {%0, %1, %2, %3}, [%4];"
-                         : "=r"(data.x), "=r"(data.y), "=r"(data.z), "=r"(data.w) : "l"(src));
-            packed<T, N> out;
-            int4* ptr = (int4*)out.data;
-            *ptr = data;
-            return out;
+            int4 data = __ldcs((const int4*)src);
+            return *(packed<T, N>*)&data;
         } else if constexpr (sizeof(T) * N == 8) {
-            int2 data = __ldcg((const int2*)src);
+            int2 data = __ldcs((const int2*)src);
             return *(packed<T, N>*)&data;
         } else if constexpr (sizeof(T) * N == 4) {
-            int data = __ldcg((const int*)src);
+            int data = __ldcs((const int*)src);
             return *(packed<T, N>*)&data;
         }
     }
     return *src;
 }
 
-constexpr bool sideaware_output = false;
-constexpr int parallel_iterations = 4;
-constexpr int vec_size = 4;
 typedef unsigned int o0;
 typedef unused o1;
 typedef unsigned int i0;
 typedef unused i1;
 typedef unused i2;
 typedef unused i3;
+constexpr bool input_evict[4] = {true, true, true, true};
+constexpr bool input_discard[4] = {false, false, false, false};
+
+constexpr bool reverse_order = true;
+constexpr bool sideaware_for_o0 = false;
+constexpr int parallel_iterations = 4;
+constexpr int vec_size = 4;
 
 typedef packed<o0, vec_size> vo0;
 typedef packed<o1, vec_size> vo1;
@@ -70,11 +68,23 @@ typedef packed<i1, vec_size> vi1;
 typedef packed<i2, vec_size> vi2;
 typedef packed<i3, vec_size> vi3;
 
+__device__ __forceinline__ void discard_inputs(
+    const vi0 * input0, const vi1 * input1, const vi2 * input2, const vi3 * input3) {
+    if (input_discard[0] && !is_same<i0, unused>::value && (unsigned long long)input0 % 128 == 0)
+        asm volatile("discard.global.L2 [%0], 128;\n" : : "l"(input0));
+    if (input_discard[1] && !is_same<i1, unused>::value && (unsigned long long)input1 % 128 == 0)
+        asm volatile("discard.global.L2 [%0], 128;\n" : : "l"(input1));
+    if (input_discard[2] && !is_same<i2, unused>::value && (unsigned long long)input2 % 128 == 0)
+        asm volatile("discard.global.L2 [%0], 128;\n" : : "l"(input2));
+    if (input_discard[3] && !is_same<i3, unused>::value && (unsigned long long)input3 % 128 == 0)
+        asm volatile("discard.global.L2 [%0], 128;\n" : : "l"(input3));
+}
+
 // ---------------------------------------------------------------------------------
 // L2 Side Aware Multi-Input / Multi-Output Elementwise kernel (requires 16B alignment)
 // ---------------------------------------------------------------------------------
 
-template<typename size_type = size_t>
+template<typename size_type = long long>
 __device__ __forceinline__ void elementwise_op(
         size_type element_idx, o0 &output0, o1 &output1,
         const i0 &input0, const i1 &input1, const i2 &input2, const i3 &input3) {
@@ -83,7 +93,7 @@ __device__ __forceinline__ void elementwise_op(
     output1 = (o1)(input1);
 }
 
-template<typename size_type = size_t>
+template<typename size_type = long long>
 __device__ __forceinline__ void vector_op(
         size_type vec_idx, vo0 &output0, vo1 &output1,
         const vi0 &input0, const vi1 &input1, const vi2 &input2, const vi3 &input3) {
@@ -93,7 +103,7 @@ __device__ __forceinline__ void vector_op(
     }
 }
 
-template<typename size_type = size_t>
+template<typename size_type = long long>
 __device__ __forceinline__ void side_aware_elementwise_device(
         vo0* __restrict__ output0, vo1* __restrict__ output1,
         const vi0 * __restrict__ input0, const vi1 * __restrict__ input1,
@@ -114,20 +124,23 @@ __device__ __forceinline__ void side_aware_elementwise_device(
 
     // blockDim.x * element_size must equal CHUNK_SIZE (we check this on the host side)
     // e.g. 256 threads * 16 bytes = 4096 bytes
-    constexpr size_type element_size = sideaware_output ? sizeof(vo0) : sizeof(vi0);
+    constexpr size_type element_size = sideaware_for_o0 ? sizeof(vo0) : sizeof(vi0);
 
-    unsigned int group = threadIdx.y;
-    unsigned int group_tid_offset = threadIdx.x * element_size;
-    unsigned int num_groups_per_side = num_sm_per_side * NUM_GROUPS;
-    unsigned int global_idx = (sm_side_index * NUM_GROUPS) + group;
+    int group = threadIdx.y;
+    int group_tid_offset = threadIdx.x * element_size;
+    int num_groups_per_side = num_sm_per_side * NUM_GROUPS;
 
-    unsigned int num_double_chunks = (num_elements * element_size) / (2 * CHUNK_SIZE);
-    unsigned int multi_chunks = num_double_chunks / parallel_iterations;
-    unsigned int base = (sideaware_output ? (unsigned long long)output1 : (unsigned long long)input0) & 0xFFFFFFFFULL;
+    int num_double_chunks = (num_elements * element_size) / (2 * CHUNK_SIZE);
+    int multi_chunks = num_double_chunks / parallel_iterations;
 
-    unsigned int offset_per_group = (parallel_iterations * CHUNK_SIZE * 2);
-    unsigned int offset_outer_loop = (offset_per_group * num_groups_per_side) - offset_per_group;
-    size_type byte_offset = global_idx * offset_per_group + group_tid_offset;
+    size_type offset_per_group = (parallel_iterations * CHUNK_SIZE * 2);
+    size_type offset_outer_loop = (offset_per_group * num_groups_per_side);
+    offset_outer_loop = (reverse_order ? -offset_outer_loop : offset_outer_loop) - offset_per_group;
+
+    int global_idx = (sm_side_index * NUM_GROUPS) + group;
+    int adjusted_global_idx = reverse_order ? (multi_chunks - global_idx - 1) : global_idx;
+    size_type byte_offset = adjusted_global_idx * offset_per_group + group_tid_offset;
+    unsigned int base = (sideaware_for_o0 ? (unsigned long long)output0 : (unsigned long long)input0) & 0xFFFFFFFFULL;
 
     size_type elements[parallel_iterations];
     vi0 inputs0[parallel_iterations];
@@ -138,7 +151,8 @@ __device__ __forceinline__ void side_aware_elementwise_device(
     vo1 out1;
     int j;
 
-    for (unsigned int i = global_idx; i < multi_chunks; i += num_groups_per_side, byte_offset += offset_outer_loop) {
+    #pragma unroll 1
+    for (int i = global_idx; i < multi_chunks; i += num_groups_per_side, byte_offset += offset_outer_loop) {
         #pragma unroll parallel_iterations
         for (j = 0; j < parallel_iterations; j++, byte_offset += 2*CHUNK_SIZE) {
             unsigned int lsb_bits = base + (byte_offset & 0xFFFFFFFF);
@@ -152,10 +166,16 @@ __device__ __forceinline__ void side_aware_elementwise_device(
 
             size_type element = offset / element_size;
             elements[j] = element;
-            inputs0[j] = input0[element];
-            inputs1[j] = input1[element];
-            inputs2[j] = input2[element];
-            inputs3[j] = input3[element];
+            inputs0[j] = load<input_evict[0]>(&input0[element]);
+            inputs1[j] = load<input_evict[1]>(&input1[element]);
+            inputs2[j] = load<input_evict[2]>(&input2[element]);
+            inputs3[j] = load<input_evict[3]>(&input3[element]);
+        }
+
+        #pragma unroll parallel_iterations
+        for (j = 0; j < parallel_iterations; j++) {
+            size_type element = elements[j];
+            discard_inputs(input0 + element, input1 + element, input2 + element, input3 + element);
         }
 
         #pragma unroll parallel_iterations
@@ -182,10 +202,12 @@ __device__ __forceinline__ void side_aware_elementwise_device(
             size_type element = byte_offset / element_size;
             if (element < num_elements) {
                 j = 0;
-                inputs0[j] = input0[element];
-                inputs1[j] = input1[element];
-                inputs2[j] = input2[element];
-                inputs3[j] = input3[element];
+                inputs0[j] = load<input_evict[0]>(input0 + element);
+                inputs1[j] = load<input_evict[1]>(input1 + element);
+                inputs2[j] = load<input_evict[2]>(input2 + element);
+                inputs3[j] = load<input_evict[3]>(input3 + element);
+                discard_inputs(input0 + element, input1 + element, input2 + element, input3 + element);
+
                 vector_op(element, out0, out1, inputs0[j], inputs1[j], inputs2[j], inputs3[j]);
                 output0[element] = out0;
                 output1[element] = out1;
@@ -203,14 +225,14 @@ extern "C" {
 __global__ __launch_bounds__(1024, 1) void side_aware_memcpy_32(vo0* __restrict__ dst, const vi0* __restrict__ src,
         unsigned int num_elements, unsigned int sm_per_side, __grid_constant__ const param_sm_side_t params) {
 
-    side_aware_elementwise_device<unsigned int>(dst, nullptr, src, nullptr, nullptr, nullptr,
+    side_aware_elementwise_device<int>(dst, nullptr, src, nullptr, nullptr, nullptr,
                                                 num_elements, sm_per_side, params);
 }
 
 __global__ __launch_bounds__(1024, 1) void side_aware_memcpy_64(vo0* __restrict__ dst, const vi0* __restrict__ src,
         size_t num_elements, unsigned int sm_per_side, __grid_constant__ const param_sm_side_t params) {
 
-    side_aware_elementwise_device<size_t>(dst, nullptr, src, nullptr, nullptr, nullptr,
+    side_aware_elementwise_device<long long>(dst, nullptr, src, nullptr, nullptr, nullptr,
                                           num_elements, sm_per_side, params);
 }
 
